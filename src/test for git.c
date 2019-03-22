1a3cb335c45e1271140f2a143254b8ea8a913d95
student <student@localhost>
2019-03-22 10:56:24 +0300

    fwdsfw

--- src/test for git.c
+++ src/test for git.c
@@ -11,7 +11,230 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+//style will be later (sorry)
+float plus ()
+{
+	float a ;
+	float b ;
+	float ret ;
+	while (2)
+	{
+	  printf("enter first number\n");
+	  ret = scanf("%f",&a);
+	  if (0 != ret)
+	      {
+	        printf("OK\n");
+	      }
+	  else
+	      {
+	        printf("invalid-not a number(or not)");
+	        break;
+	      }
+	  printf("enter second number\n");
+	  ret = scanf("%f",&b);
+	  if (0 != ret)
+	      {
+	        printf("OK\n");
+	      }
+	 else
+	      {
+	        printf("invalid-not a number(or not)");
+	        break;
+	      }
+     a = a + b;
+     printf("%f",a);
+	}
+	return 0;
+}
+
+float min()
+{
+	float a ;
+	float b ;
+	float ret ;
+	while (2)
+	{
+	  printf("enter first number\n");
+	  ret = scanf("%f",&a);
+	  if (0 != ret)
+	      {
+	        printf("OK\n");
+	      }
+	  else
+	      {
+	        printf("invalid-not a number(or not)");
+	        break;
+	      }
+	  printf("enter second number\n");
+	  ret = scanf("%f",&b);
+	  if (0 != ret)
+	      {
+	        printf("OK\n");
+	      }
+	 else
+	      {
+	        printf("invalid-not a number(or not)");
+	        break;
+	      }
+     a = a - b;
+     printf("%f",a);
+	}
+	return 0;
+}
+
+unsigned int fact()
+{
+	unsigned int ret;
+	puts ("enter a number");
+	unsigned int a ;
+	unsigned int b = 1;
+	while (2)
+		{
+		printf("enter first number\n");
+		ret = scanf("%u",&a);
+		if (0 != ret)
+	      	  {
+			printf("OK\n");
+	      	  }
+		else
+			{
+			printf("invalid-not a number(or not)");
+	        break;
+			}
+for (int i = 1;i<=a;i++)
+{
+b=b*i;
+}
+printf("%u",b);
+}
+	return 0;
+}
+
+float multiplication()
+{
+	float a;
+	float b;
+	float ret ;
+	while (2)
+	{
+	  printf("enter first number\n");
+	  ret = scanf("%f",&a);
+	  if (0 != ret)
+	      {
+	        printf("OK\n");
+	      }
+	  else
+	      {
+	        printf("invalid-not a number(or not)");
+	        break;
+	      }
+	  printf("enter second number\n");
+	  ret = scanf("%f",&b);
+	  if (0 != ret)
+	      {
+	        printf("OK\n");
+	      }
+	 else
+	      {
+	        printf("invalid-not a number(or not)");
+	        break;
+	      }
+a= a*b;
+printf("%f",b);
+return 0;
+}
+
+float division()
+{
+	float ret ;
+		while (2)
+		{
+		  printf("enter first number\n");
+		  ret = scanf("%f",&a);
+		  if (0 != ret)
+		      {
+		        printf("OK\n");
+		      }
+		  else
+		      {
+		        printf("invalid-not a number(or not)");
+		        break;
+		      }
+		  printf("enter second number\n");
+		  ret = scanf("%f",&b);
+		  if (0 != ret)
+		      {
+		        printf("OK\n");
+		      }
+		 else
+		      {
+		        printf("invalid-not a number(or not)");
+		        break;
+		      }
+a= a/b;
+printf("%f",b);
+return 0;
+}
+
+float power()
+{
+	float ret ;
+		while (2)
+		{
+		  printf("enter  number\n");
+		  ret = scanf("%f",&a);
+		  if (0 != ret)
+		      {
+		        printf("OK\n");
+		      }
+		  else
+		      {
+		        printf("invalid-not a number(or not)");
+		        break;
+		      }
+		  printf("enter his power\n");
+		  ret = scanf("%f",&b);
+		  if (0 != ret)
+		      {
+		        printf("OK\n");
+		      }
+		 else
+		      {
+		        printf("invalid-not a number(or not)");
+		        break;
+		      }
+for (int i = 1;i<=b;i++)
+{
+a*=a;
+}
+printf("%f",b);
+return 0;
+}
+
+
+
+int main()
+{
+char Y = 'Y';
+int ok = 0;
+char ch ='p';
+puts("do u need a list of commandsY/N");
+scanf("%c",&ch);
+if (ch==Y) puts("+_f^");
+puts("enter a commabd");
+while (ok!=1)
+{
+scanf("%c",&ch);
+switch(ch)
+{
+case '+' :plus() ; ok =ok+1;break;
+case '-' :puts("2"); break;
+case 'f' :puts("3"); break;
+case '*' :puts("4"); break;
+case '/' :puts("5"); break;
+case '^' :puts("6"); break;
+default :puts("if u entered a wrong command just do it again but correctly ");ch ='l';
+}
+}
+return 0;
 }
