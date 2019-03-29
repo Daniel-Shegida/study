67e34c29ef98d11e026be4f460808001ea0a4032
ECTOBIOLOBAJUN <47796424+ECTOBIOLOBAJUN@users.noreply.github.com>
2019-03-29 10:21:02 +0300

    Update test for git.c

--- src/test for git.c
+++ src/test for git.c
@@ -6,242 +6,233 @@
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
- */
-
-/*#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 
 //style will be later (sorry)
-float plus ()
+float plus_print ()
 {
 	float a ;
 	float b ;
 	float ret ;
-	while (2)
+	printf("enter first number\n");
+	ret = scanf("%f",&a);
+	if (0 != ret)
 	{
-	  printf("enter first number\n");
-	  ret = scanf("%f",&a);
-	  if (0 != ret)
-	      {
-	        printf("OK\n");
-	      }
-	  else
-	      {
-	        printf("invalid-not a number(or not)");
-	        break;
-	      }
-	  printf("enter second number\n");
-	  ret = scanf("%f",&b);
-	  if (0 != ret)
-	      {
-	        printf("OK\n");
-	      }
-	 else
-	      {
-	        printf("invalid-not a number(or not)");
-	        break;
-	      }
-     a = a + b;
-     printf("%f",a);
+		printf("OK\n");
 	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+	    return 0;
+	}
+	printf("enter second number\n");
+	ret = scanf("%f",&b);
+	if (0 != ret)
+    {
+		printf("OK\n");
+	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+	    return 0;
+	}
+    a = a + b;
+    printf("%f",a);
 	return 0;
 }
 
-float min()
+float min_print()
 {
 	float a ;
 	float b ;
 	float ret ;
-	while (2)
+	printf("enter first number\n");
+	ret = scanf("%f",&a);
+	if (0 != ret)
 	{
-	  printf("enter first number\n");
-	  ret = scanf("%f",&a);
-	  if (0 != ret)
-	      {
-	        printf("OK\n");
-	      }
-	  else
-	      {
-	        printf("invalid-not a number(or not)");
-	        break;
-	      }
-	  printf("enter second number\n");
-	  ret = scanf("%f",&b);
-	  if (0 != ret)
-	      {
-	        printf("OK\n");
-	      }
-	 else
-	      {
-	        printf("invalid-not a number(or not)");
-	        break;
-	      }
-     a = a - b;
-     printf("%f",a);
+		printf("OK\n");
 	}
+    else
+	{
+    	printf("invalid-not a number(or not)");
+	    return 0 ;
+	}
+	printf("enter second number\n");
+	ret = scanf("%f",&b);
+	if (0 != ret)
+	{
+		printf("OK\n");
+	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+	    return 0;
+	}
+    a = a - b;
+    printf("%f",a);
 	return 0;
 }
 
-unsigned int fact()
+unsigned int fact_print()
 {
 	unsigned int ret;
-	unsigned int a ;65к
+	puts ("enter a number");
+	unsigned int a ;
 	unsigned int b = 1;
-	while (2)
-		{
-		printf("enter first number\n");
-		ret = scanf("%u",&a);
-		if (0 != ret)
-	      	  {
-			printf("OK\n");
-	      	  }
-		else
-			{
-			printf("invalid-not a number(or not)");
-	        break;
-			}
-for (int i = 1;i<=a;i++)
-{
-b=b*i;
-}
-printf("%u",b);
-}
+	printf("enter first number\n");
+	ret = scanf("%u",&a);
+	if (0 != ret)
+	{
+		printf("OK\n");
+	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+	    return 0;
+	}
+	for (int i = 1;i<=a;i++)
+	{
+		b=b*i;
+	}
+	printf("%u",b);
 	return 0;
 }
 
-float multiplication()
+float multiplication_print()
 {
 	float a;
 	float b;
 	float ret ;
-	while (2)
+	printf("enter first number\n");
+	ret = scanf("%f",&a);
+	if (0 != ret)
 	{
-	  printf("enter first number\n");
-	  ret = scanf("%f",&a);
-	  if (0 != ret)
-	      {
-	        printf("OK\n");
-	      }
-	  else
-	      {
-	        printf("invalid-not a number(or not)");
-	        break;
-	      }
-	  printf("enter second number\n");
-	  ret = scanf("%f",&b);
-	  if (0 != ret)
-	      {
-	        printf("OK\n");
-	      }
-	 else
-	      {
-	        printf("invalid-not a number(or not)");
-	        break;
-	      }
-a= a*b;
-printf("%f",b);
+		printf("OK\n");
 	}
-return 0;}
+	else
+	{
+		printf("invalid-not a number(or not)");
+		return 0;
+	}
+	printf("enter second number\n");
+	ret = scanf("%f",&b);
+	if (0 != ret)
+	{
+		printf("OK\n");
+	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+	    return 0;
+	}
+	a= a*b;
+	printf("%f",b);
+	return 0;}
 
-
-float division()
+float division_print()
 {
 	float ret ;
 	float a;
 	float b;
-		while (2)
-		{
-		  printf("enter first number\n");
-		  ret = scanf("%f",&a);
-		  if (0 != ret)
-		      {
-		        printf("OK\n");
-		      }
-		  else
-		      {
-		        printf("invalid-not a number(or not)");
-		        break;
-		      }
-		  printf("enter second number\n");
-		  ret = scanf("%f",&b);
-		  if (0 != ret)
-		      {
-		        printf("OK\n");
-		      }
-		 else
-		      {
-		        printf("invalid-not a number(or not)");
-		        break;
-		      }
-a= a/b;
-printf("%f",b);}
-return 0;
+	printf("enter first number\n");
+	ret = scanf("%f",&a);
+	if (0 != ret)
+	{
+		printf("OK\n");
+	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+		return 0;
+	}
+	printf("enter second number\n");
+	ret = scanf("%f",&b);
+	if (0 != ret)
+	{
+		printf("OK\n");
+	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+		return 0;
+	}
+	a= a/b;
+	printf("%f",a);
+	return 0;
 }
 
-float power()
+float power_print()
 {
 	float ret ;
 	float a;
 	int b;
-		while (2)
-		{
-		  printf("enter  number\n");
-		  ret = scanf("%f",&a);
-		  if (0 != ret)
-		      {
-		        printf("OK\n");
-		      }
-		  else
-		      {
-		        printf("invalid-not a number(or not)");
-		        break;
-		      }
-		  printf("enter his power\n");
-		  ret = scanf("%i",&b);
-		  if (0 != ret)
-		      {
-		        printf("OK\n");
-		      }
-		 else
-		      {
-		        printf("invalid-not a number(or not)");
-		        break;
-		      }
-for (int i = 1;i<=b;i++)
-{
-a*=a;
+	printf("enter  number\n");
+	ret = scanf("%f",&a);
+	if (0 != ret)
+	{
+		printf("OK\n");
+	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+		return 0;
+	}
+	printf("enter his power\n");
+	ret = scanf("%i",&b);
+	if (0 != ret)
+	{
+		printf("OK\n");
+	}
+	else
+	{
+		printf("invalid-not a number(or not)");
+		return 0;
+	}
+	for (int i = 1;i<=b;i++)
+	{
+		a*=a;
+	}
+	printf("%f",b);
+	return 0;
 }
-
-printf("%f",b);}
-return 0;
-}
-das
 
 
 int main()
 {
-char Y = 'Y';
-int ok = 0;
-char ch ='p';
-puts("do u need a list of commandsY/N");
-scanf("%c",&ch);
-if (ch==Y) puts("+_f^");
-puts("enter a commabd");
-while (ok!=1)
-{
-scanf("%c",&ch);
-switch(ch)
-{
-case '+' :plus() ; ok =ok+1;break;
-case '-' :puts("2"); break;
-case 'f' :puts("3"); break;
-case '*' :puts("4"); break;
-case '/' :puts("5"); break;
-case '^' :puts("6"); break;
-default :puts("if u entered a wrong command just do it again but correctly ");ch ='l';
+	char Y = 'Y';
+	int ok = 0;
+	int q = 34;
+	char ch ='p';
+	puts("do u need a list of commandsY/N");
+	scanf("%c",&ch);
+	if (ch==Y) puts("+_f^");
+	puts("enter a command");
+	while (ok!=1)
+	{
+			scanf("%c",&ch);
+			switch(ch)
+			{
+				case '+' :plus_print() ; puts("do u want to repeat");scanf("%i",&q);
+				if (q==0) {ok+=1;break;}
+				else break;
+				case '-' :min_print(); puts("do u want to repeat");scanf("%i",&q);
+				if (q==0) {ok+=1;break;}
+				else break;
+				case 'f' :fact_print();puts("do u want to repeat");scanf("%i",&q);
+				if (q==0) {ok+=1;break;}
+				else break;
+				case '*' :multiplication_print(); puts("do u want to repeat");scanf("%i",&q);
+				if (q==0) {ok+=1;break;}
+				else break;
+				case '/' :division_print(); puts("do u want to repeat");scanf("%i",&q);
+				if (q==0) {ok+=1;break;}
+				else break;
+				case '^' :power_print(); puts("do u want to repeat");scanf("%i",&q);
+				if (q==0) {ok+=1;break;}
+				else break;
+				default :puts("another cycle don't forget commands +-f^*/  ");ch ='l';break;
+			}
+	}
+	return 0;
 }
-}
-return 0;
-}123123124124124233412131232*/
-int main()
-{return 0;}
